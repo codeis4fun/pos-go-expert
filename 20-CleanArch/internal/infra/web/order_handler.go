c1b0e29e14e1a4ba39ecc4853462628a8f8a61a1
codeis4fun <nicholascarballo@icloud.com>
2023-08-30 16:31:26 -0300

    feat: Add new handler to list orders

--- 20-CleanArch/internal/infra/web/order_handler.go
+++ 20-CleanArch/internal/infra/web/order_handler.go
@@ -3,6 +3,7 @@ package web
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/devfullcycle/20-CleanArch/internal/entity"
 	"github.com/devfullcycle/20-CleanArch/internal/usecase"
@@ -41,6 +42,43 @@ func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *WebOrderHandler) List(w http.ResponseWriter, r *http.Request) {
+	var dto usecase.ListOrdersInputDTO
+
+	queryValues := r.URL.Query()
+	page := queryValues.Get("page")
+	limit := queryValues.Get("limit")
+
+	pageNumber, err := strconv.Atoi(page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	limitNumber, err := strconv.Atoi(limit)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dto.Page = pageNumber
+	dto.Limit = limitNumber
+
+	listOrders := usecase.NewListOrdersUseCase(h.OrderRepository)
+	output, err := listOrders.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(w).Encode(output)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
